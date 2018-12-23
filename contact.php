6de31de6504726b8019e070505fa70e9795c090f
spark22 <sparkly6681@gmail.com>
2018-12-23 15:50:57 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
         <?php
         
         if($_POST["submit"]) {
-            $recipient="your@email.address";
+            $recipient="cynidewandia@gmail.com";
             $subject="Form to email message";
             $sender=$_POST["sender"];
             $senderEmail=$_POST["senderEmail"];
